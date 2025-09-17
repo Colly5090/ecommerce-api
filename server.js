9b372622dc662f99a4b5e086f88c7c8d847f332f
Collins Boakye <colly0087@gmail.com>
2025-09-17 07:06:52 -0700

    added

--- server.js
+++ server.js
@@ -7,7 +7,5 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use("/api", router);
 
-// Export request handler for Vercel
-module.exports = (req, res) => {
-  server(req, res);
-};
+// Vercel needs a function export, not .listen()
+module.exports = server;
