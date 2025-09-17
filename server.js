ee206c23d3d13239f37368b861a798339dd729c2
Collins Boakye <colly0087@gmail.com>
2025-09-17 06:48:48 -0700

    added

---
+++ server.js
@@ -0,0 +1,11 @@
+// server.js
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use("/api", router); // Prefix all endpoints with /api
+
+// Export for Vercel
+module.exports = server;
